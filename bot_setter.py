cc30ceed8f12fbc8c83300b4b199b050a7ff18a4
Dmytro Oseledko <d.oseledko@outlook.com>
2023-01-18 22:55:02 +0200

    done

--- bot_setter.py
+++ bot_setter.py
@@ -18,28 +18,112 @@ def input_error(function):
 
 
 class AddressBook(UserDict):
-    pass
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+
+    def iterator(self, page_number, num_of_records):
+        new_data = list(self.data.items())
+        total_items = page_number * num_of_records
+        yield list(new_data[(total_items - num_of_records):total_items])
 
 
 class Record:
-    pass
+    def __init__(self, new_name):
+        self.name = Name(new_name)
+        self.phones = []
+        self.birthday = None
+
+
+    @input_error
+    def add_phone(self, new_phone):
+        add_phone = Phone()
+        add_phone.value = new_phone
+        self.phones.append(add_phone)
+
+
+    @input_error
+    def change_phone(self, old_phone, new_phone):
+        for phone in self.phones:
+            if phone.value == old_phone:
+                new_pn = Phone()
+                new_pn.value = new_phone
+                self.phones.append(new_pn)
+                self.phones.remove(phone)
+        
+
+
+    def remove_phone(self, old_phone):
+        for phone in self.phones:
+            if phone.value == old_phone:
+                self.phones.remove(phone)
+        else:
+            print("Phone number does't exist")
+
+
+    @input_error
+    def add_birthday(self, birthday):
+        bday = Birthday()
+        bday.value = birthday
+        self.birthday = bday
+
+
+    def days_to_birthday(self):
+        if self.birthday:
+            today = datetime.now().date()
+            if self.birthday.value.replace(year=today.year) >= today:
+                result = self.birthday.value.replace(year=today.year) - today
+            else:
+                result = self.birthday.value.replace(
+                    year=today.year) - today.replace(year=today.year - 1)
+            print(result)
+        else:
+            print('Empty')
+
+
+    def __repr__(self):
+        return f'{self.phones}'
 
 
 class Field:
-    pass
+    def __init__(self):
+        self.value = None
+
+
+    @property
+    def value(self):
+        return self._value
+
+
+    @value.setter
+    def value(self, value):
+        self._value = value
 
 
 class Name(Field):
-    pass
+    def __init__(self, name) -> None:
+        self.value = name
 
 
 class Phone(Field):
-    pass
+    @Field.value.setter
+    def value(self, value):
+        if len(value) < 10 or len(value) > 12:
+            raise ValueError("Phone must contains 10 symbols.")
+        if not value.isnumeric():
+            raise ValueError('Wrong phones.')
+        self._value = value
 
 
     
 class Birthday(Field):
-    pass
+    @Field.value.setter
+    def value(self, value):
+        today = datetime.now().date()
+        birth_date = datetime.strptime(value, '%Y-%m-%d').date()
+        if birth_date > today:
+            raise ValueError("Birthday must be less than current year and date.")
+        self._value = value
 
 
 addressbook = AddressBook()
@@ -47,7 +131,8 @@ addressbook = AddressBook()
 
 @input_error
 def get_user_input():
-    pass
+    user_input = input('Enter command: ').lower().split(' ')
+    return user_input
 
 
 @input_error
@@ -61,46 +146,86 @@ def hello_func(*args):
 
 @input_error
 def add_func(user_input):
-    pass
+    if user_input[1] not in addressbook.data:
+        add_record = Record(user_input[1])
+        add_record.add_phone(user_input[2])
+        addressbook.add_record(add_record)
+        print(f'New contact added')
+    else:
+        add_phone = addressbook.data[user_input[1]]
+        add_phone.add_phone(user_input[2])
+        print(f'New phone number to {user_input[1]} has been added')
 
 
 @input_error
 def change_func(user_input):
-    pass
+    if user_input[1] in addressbook.data:
+        old_phone = input('Enter phone number to change: ')
+        renew_phone = addressbook.data[user_input[1]]
+        renew_phone.change_phone(old_phone, user_input[2])
+        print(f'Phone number {old_phone} has been changed to {user_input[2]}')
+    else:
+        print(f"Contact {user_input[1]} doesn't exist")
 
 
 @input_error
 def phone_func(user_input):
-    pass
+    if user_input[1] in addressbook.data:
+        print(
+            f'{user_input[1]} has {addressbook.data[user_input[1]]} phone number')
+    else:
+        print(f"Contact '{user_input[1]}' doesn't exist")
 
 
 @input_error
 def delete_func(user_input):
-    pass
+    if user_input[1] in addressbook.data:
+        addressbook.data.pop(user_input[1])
+        print(f'Contact "{user_input[1]}" has been deleted')
+    else:
+        print(f"Contact '{user_input[1]}' doesn't exist")
 
 
 @input_error
 def remove_phone_func(user_input):
-    pass
+    if user_input[1] in addressbook.data:
+        removing = addressbook.data[user_input[1]]
+        removing.remove_phone(user_input[2])
+        print(f'Phone number "{user_input[2]}" has been removed')
+    else:
+        print(f"Contact '{user_input[1]}' doesn't exist")
 
 
 @input_error
 def set_birthday(user_input):
-    pass
+    if user_input[1] in addressbook.data:
+        setbday = addressbook.data[user_input[1]]
+        setbday.add_birthday(user_input[2])
+        print(f'Birthday {user_input[2]} has added to {user_input[1]}')
+    else:
+        print(f"Contact '{user_input[1]}' doesn't exist")
 
 
 @input_error
 def show_birthday(user_input):
-    pass
+    if user_input[1] in addressbook.data:
+        addressbook.data[user_input[1]].days_to_birthday()
+    else:
+        print(f"Contact '{user_input[1]}' doesn't exist")
 
 
 @input_error
 def show_all_func(*args):
-    pass
+    page_number = int(input('Enter page number: '))
+    num_of_records = int(input('How many records we need: '))
+    page = addressbook.iterator(page_number, num_of_records)
+    print(next(page))
 
 
 def break_func(*a):   
-    pass
+    result = 'Good bye, see you soon:)'
+    print('Good bye, see you soon:)')
+    return result
 
 
 OPERATIONS = {
@@ -120,7 +245,24 @@ OPERATIONS = {
 
 
 def main():
-    pass
+    print('Hello, your assistants start working)')
+    while True:
+        user_input = get_user_input()
+
+        if user_input == ['']:
+            continue
+        elif user_input[0] == 'show':
+            actions = 'show all'
+        else:
+            actions = user_input[0]
+
+        handler = get_handler(actions)
+        if handler is None:
+            continue
+
+        result = handler(user_input)
+        if result == 'Good bye, see you soon:)':
+            break
 
 
 if __name__ == '__main__':
